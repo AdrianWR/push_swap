aee71b0fc046a5f5b4091474fb99195554eb2d5c
Adrian Roque <adrian.w.roque@gmail.com>
2021-04-18 15:29:24 -0300

    Fix linter issues

--- include/general.h
+++ include/general.h
@@ -6,11 +6,14 @@
 /*   By: aroque <aroque@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 21:00:27 by aroque            #+#    #+#             */
-/*   Updated: 2021/04/18 15:13:57 by aroque           ###   ########.fr       */
+/*   Updated: 2021/04/18 15:29:10 by aroque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "stack.h"
+#ifndef GENERAL_H
+# define GENERAL_H
+
+# include "stack.h"
 
 # define SPACE ' '
 
@@ -18,5 +21,7 @@ bool	atoiv(const char *str, int *overflow);
 void	free_array(void **array);
 void	free_stack(t_stack *stack);
 void	message_and_exit(t_stack *stack, char **ops, int status);
-t_stack *get_stack(int size, char **args);
 void	reverse_array(int *array, size_t size);
+t_stack	*get_stack(int size, char **args);
+
+#endif
